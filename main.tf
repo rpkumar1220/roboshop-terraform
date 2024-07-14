b568197f5c2a9457275ee675fe1f56db68ca52c5
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2024-07-14 21:48:31 +0530

    Setting up roboshop project with Terraform

--- main.tf
+++ main.tf
@@ -3,10 +3,7 @@
 module "vpc" {
   source = "git::https://github.com/rpkumar1220/vpc-module.git"
 
-
   for_each = var.vpc
-  vpc_id = var.vpc
-
   cidr_block=each.value["cidr_block"]
   subnets=each.value["subnets"]
 
