552073bed0929d70e19f7a5a73a6c5f84b1fe332
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2024-07-16 16:13:41 +0530

    Setting up roboshop project with Terraform

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ module "vpc" {
   for_each = var.vpc
   cidr_block=each.value["cidr_block"]
   subnets=each.value["subnets"]
+  default_vpc_id=var.default_vpc_id
+  default_vpc_rt=var.default_vpc_rt
 
   tags=var.tags
   env= var.env
